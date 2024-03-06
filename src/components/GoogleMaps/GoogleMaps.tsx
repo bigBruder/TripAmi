4a337533e83f69ae6e8c5f463246aea6c080219a
vysochanskyi <visosensey@gmail.com>
2024-03-06 17:47:21 +0200

    implemented google map

--- src/components/GoogleMaps/GoogleMaps.tsx
+++ src/components/GoogleMaps/GoogleMaps.tsx
@@ -1,31 +1,125 @@
-import styles from './googleMaps.module.css';
-import MapOrange from '@assets/icons/MapOrange.svg';
-import Build from '@assets/icons/build.svg';
-import {useMemo} from "react";
-import {GoogleMap} from "@react-google-maps/api";
+import { useContext, useEffect, useState } from "react";
+import {
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  Pin,
+  InfoWindow,
+} from "@vis.gl/react-google-maps";
+import React from "react";
+import useTravelsContext from "../TravelItinerary/store";
+import { getDocs, onSnapshot, orderBy, query, where } from "@firebase/firestore";
+import {postsCollection, tripsCollection} from "../../types/firestoreCollections";
+import {IPost} from "../../types/post";
+import {AuthContext} from "../../providers/authContext";
+import {ITravel} from "../../types/travel";
+import TravelCard from "../TravelCard/TravelCard";
 
-const GoogleMaps = () => {
-  const center = useMemo(() => ({ lat: 18.52043, lng: 73.856743 }), []);
+
+export default function Intro() {
+  const position = { lat: 53.54, lng: 10 };
+  const [open, setOpen] = useState(false);
+  const {travels, setTravels} = useTravelsContext();
+  const [isSuggestedPostsLoading, setIsSuggestedPostsLoading] = useState(false);
+  const [suggestedPosts, setSuggestedPosts] = useState<IPost[] | null>(null);
+  const {firestoreUser} = useContext(AuthContext);
+  const [selectedTravel, setSelectedTravel] = useState<ITravel | null>(null);
+  
+
+  useEffect(() => {
+      const q = query(tripsCollection, where('userId', '==', firestoreUser?.id));
+      const unsub = onSnapshot(q, (querySnapshot) => {
+        const fetchedTravel = querySnapshot.docs.map(doc => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setTravels(fetchedTravel as ITravel[]);
+      });
+
+
+
+      (async () => {
+        try {
+          setIsSuggestedPostsLoading(true);
+          const q = query(
+            postsCollection,
+            orderBy('userId'),
+            where('userId', '!=', firestoreUser?.id),
+            orderBy('createAt', 'desc'),
+          );
+          const querySnapshot = await getDocs(q);
+          const fetchedPosts = querySnapshot.docs.map(doc => ({
+            ...doc.data(),
+            id: doc.id,
+          }));
+
+          setSuggestedPosts(fetchedPosts as IPost[]);
+        } catch (err) {
+          console.log(err);
+          // @ts-ignore
+          alert(firebaseErrors[err.code]);
+        } finally {
+          setIsSuggestedPostsLoading(false);
+        }
+      })();
+
+      return () => {
+        unsub();
+      }
+  }, [firestoreUser?.id, ]);
+
+
+
+  // console.log(travels);
+  // console.log(typeof (travels[0].location.latitude));
 
   return (
-    <div className={styles.container}>
-      <div className={styles.titleContainer}>
-        <p className={styles.title}>Build a travel itinerary <img src={MapOrange} /></p>
-        <p className={styles.title}>Build a travel itinerary based on other people's reviews</p>
-      </div>
-      <div className={styles.subtitle}>
-        <p className={styles.title}>Build a route based on user reviews <img src={Build} /></p>
-      </div>
+    <APIProvider apiKey="AIzaSyCwDkMaHWXRpO7hY6z62_Gu8eLxMMItjT8">
+      <div style={{ height: "100vh", width: "100%" }}>
 
-      <div className={styles.mapsWrapper}>
-        <GoogleMap
-          mapContainerClassName={styles.mapContainer}
-          center={center}
-          zoom={3}
-        />
+        <Map defaultZoom={5} defaultCenter={position} mapId="9bc3b1605395203e">
+          {
+          travels.length > 0 && (
+            travels?.map(travel => {
+              console.log(travel.id);
+              
+              return (
+              <AdvancedMarker 
+                position={{lat: travel.location.latitude, lng:travel.location.longitude}}
+                onClick={() => {
+                  setSelectedTravel(travel);
+                  setOpen(true);
+                } 
+              }
+                key={travel.id}
+              >
+                <Pin
+                  background={"grey"}
+                  borderColor={"green"}
+                  glyphColor={"purple"}
+                />
+              </AdvancedMarker>
+          )
+            }
+          
+           
+           ))
+          }
+          {/* <AdvancedMarker position={position} onClick={() => setOpen(true)}>
+            <Pin
+              background={"grey"}
+              borderColor={"green"}
+              glyphColor={"purple"}
+            />
+          </AdvancedMarker> */}
+
+          {selectedTravel && open && (
+            <InfoWindow position={{lat: selectedTravel.location.latitude, lng:selectedTravel.location.longitude}} onCloseClick={() => setOpen(false)}>
+              <TravelCard travel={selectedTravel}/>
+            </InfoWindow>
+          )}
+        </Map>
       </div>
-    </div>
+    </APIProvider>
   );
-};
-
-export default GoogleMaps;
+}
