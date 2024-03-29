3b2f9bf897f1066d50f32e1b2213690b4d1be89f
vysochanskyi <visosensey@gmail.com>
2024-03-29 11:43:30 +0200

    code refactor, fixed blight of photos after getting new notification

--- src/components/Posts/index.tsx
+++ src/components/Posts/index.tsx
@@ -1,40 +1,38 @@
-import styles from "./posts.module.css";
-import {IPost} from "~/types/post";
-import {ref, getDownloadURL} from "firebase/storage";
-import {FC, useContext, useEffect, useMemo, useState} from "react";
-import {storage} from "~/firebase";
-import './styles.css';
-import {usePost} from "~/hooks/post/usePost";
-import {AuthContext} from "~/providers/authContext";
-import {getDocs, query, where} from "@firebase/firestore";
-import {usersCollection} from "~/types/firestoreCollections";
-import {IUser} from "~/types/user";
-import {useNavigate} from "react-router-dom";
-import {PostActions} from "~/components/PostActions";
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
+import { FC, useContext, useEffect, useMemo, useState } from 'react';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+import { useNavigate } from 'react-router-dom';
+
+import { getDownloadURL, ref } from 'firebase/storage';
 import 'swiper/css/pagination';
 import { Pagination } from 'swiper/modules';
-import {UserPostInfo} from "~/components/BigPost/UserPostInfo";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { UserPostInfo } from '~/components/BigPost/UserPostInfo';
+import { PostActions } from '~/components/PostActions';
+import { storage } from '~/firebase';
+import { usePost } from '~/hooks/post/usePost';
+import { AuthContext } from '~/providers/authContext';
+import { usersCollection } from '~/types/firestoreCollections';
+import { IPost } from '~/types/post';
+import { IUser } from '~/types/user';
+
+import { getDocs, query, where } from '@firebase/firestore';
+
+import styles from './posts.module.css';
+import './styles.css';
+
+import 'swiper/css';
 
 interface Props {
   postData: IPost;
 }
 
-const PostItem: FC<Props> = ({postData}) => {
+const PostItem: FC<Props> = ({ postData }) => {
   const [imageUrl, setImageUrl] = useState<string[] | null>(null);
   const [userData, setUserData] = useState<IUser | null>(null);
-  const {
-    imageUrls,
-    createAt,
-    userId,
-    text,
-    id,
-  } = postData;
-  const {firestoreUser} = useContext(AuthContext);
-  const {handleDeletePost, isLoading, setIsLoading, handleLikePost} = usePost(id);
+  const { imageUrls, createAt, userId, text, id } = postData;
+  const { firestoreUser } = useContext(AuthContext);
+  const { handleDeletePost, isLoading, setIsLoading, handleLikePost } = usePost(id);
   const navigate = useNavigate();
   const [userPhotoUrl, setUserPhotoUrl] = useState<string>();
 
@@ -61,7 +59,7 @@ const PostItem: FC<Props> = ({postData}) => {
 
           for (let i = 0; i < imageUrls.length; i++) {
             const url = await getDownloadURL(ref(storage, imageUrls[i]));
-            imagesUrls.push(url)
+            imagesUrls.push(url);
           }
 
           setImageUrl(imagesUrls);
@@ -74,14 +72,14 @@ const PostItem: FC<Props> = ({postData}) => {
     } else {
       setIsLoading(false);
     }
-  }, [imageUrls, setIsLoading]);
+  }, [imageUrls.join(), setIsLoading]);
 
   useEffect(() => {
     (async () => {
       if (firestoreUser?.id && firestoreUser?.id !== userId) {
         try {
           setIsLoading(true);
-          const q = query(usersCollection,where('id', '==', userId));
+          const q = query(usersCollection, where('id', '==', userId));
           const querySnapshot = await getDocs(q);
           const fetchedUser = querySnapshot.docs[0].data();
 
@@ -96,7 +94,6 @@ const PostItem: FC<Props> = ({postData}) => {
   }, [firestoreUser?.firebaseUid, firestoreUser?.id, setIsLoading, userId]);
 
   const isPostMy = useMemo(() => firestoreUser?.id === userId, [firestoreUser?.id, userId]);
-
 
   return (
     <div className={styles.cardContainer}>
@@ -117,36 +114,40 @@ const PostItem: FC<Props> = ({postData}) => {
       <div className={styles.header}>
         {!imageUrls?.[0]?.length || !imageUrls?.length ? (
           <span className={styles.captionWithoutImage}>{text}</span>
-        ) : <span className={styles.caption}>{text}</span>}
+        ) : (
+          <span className={styles.caption}>{text}</span>
+        )}
       </div>
 
       <Swiper
         spaceBetween={0}
         slidesPerView={1}
-        style={{width: '100%'}}
+        style={{ width: '100%' }}
         wrapperClass={styles.swiperWrapper}
         pagination={true}
         modules={[Pagination]}
       >
-        {imageUrl?.map(link => (
-          <SwiperSlide key={link} style={{display: 'flex', justifyContent: 'center'}}>
+        {imageUrl?.map((link) => (
+          <SwiperSlide key={link} style={{ display: 'flex', justifyContent: 'center' }}>
             <img
               className={styles.img}
               src={link || ''}
-              alt="img"
+              alt='img'
               onLoadedData={() => setIsLoading(false)}
-              style={{height: "200px", width: "100%",objectFit: "cover"}}
+              style={{ height: '200px', width: '100%', objectFit: 'cover' }}
             />
           </SwiperSlide>
         ))}
       </Swiper>
 
-      {isLoading && <Skeleton className={styles.loader}/>}
+      {isLoading && <Skeleton className={styles.loader} />}
 
-      <PostActions postData={{
-        ...postData,
-        imageUrls: [...imageUrl || ''],
-      }} />
+      <PostActions
+        postData={{
+          ...postData,
+          imageUrls: [...(imageUrl || '')],
+        }}
+      />
     </div>
   );
 };
