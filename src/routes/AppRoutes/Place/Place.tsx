dbdb5e10fa2a02126595250745d0e48e4269bf0f
vysochanskyi <visosensey@gmail.com>
2024-04-02 09:32:25 +0300

    added loader for place

--- src/routes/AppRoutes/Place/Place.tsx
+++ src/routes/AppRoutes/Place/Place.tsx
@@ -1,21 +1,26 @@
-import {useParams} from "react-router-dom";
-import styles from "./place.module.css";
-import Header from "~/components/profile/Header";
-import {useEffect, useMemo, useRef, useState} from "react";
-import axios from "axios";
-import {IPlace} from "~/routes/AppRoutes/Posts/types";
-import {PageTitle} from "~/components/PageTitle";
-import Skeleton from "react-loading-skeleton";
-import {PlaceCommentField} from "~/routes/AppRoutes/Place/components/PlaceCommentField";
-import {Comment} from "~/components/Comment";
-import {IPlaceComment} from "~/types/comments";
-import {onSnapshot, orderBy, query, where} from "@firebase/firestore";
-import {placesCommentsCollection} from "~/types/firestoreCollections";
+import { useEffect, useMemo, useRef, useState } from 'react';
+import Skeleton from 'react-loading-skeleton';
+import { useParams } from 'react-router-dom';
+
+import axios from 'axios';
+import Lottie from 'lottie-react';
+import { Comment } from '~/components/Comment';
+import { PageTitle } from '~/components/PageTitle';
+import Header from '~/components/profile/Header';
+import { PlaceCommentField } from '~/routes/AppRoutes/Place/components/PlaceCommentField';
+import { IPlace } from '~/routes/AppRoutes/Posts/types';
+import { IPlaceComment } from '~/types/comments';
+import { placesCommentsCollection } from '~/types/firestoreCollections';
+
+import AnimationData from '@assets/animations/loader.json';
+import { onSnapshot, orderBy, query, where } from '@firebase/firestore';
+
+import styles from './place.module.css';
 
 const MAX_LENGTH = 700;
 
 const Place = () => {
-  const {id} = useParams();
+  const { id } = useParams();
   const [placeData, setPlaceData] = useState<IPlace | null>(null);
   const [isLoading, setIsLoading] = useState(false);
   const [isExpanded, setIsExpanded] = useState(false);
@@ -27,10 +32,10 @@ const Place = () => {
     const q = query(
       placesCommentsCollection,
       where('placeId', '==', id),
-      orderBy('createdAt', 'desc'),
+      orderBy('createdAt', 'desc')
     );
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
-      const fetchedDocs = querySnapshot.docs.map(doc => ({
+      const fetchedDocs = querySnapshot.docs.map((doc) => ({
         ...doc.data(),
         id: doc.id,
       }));
@@ -40,7 +45,7 @@ const Place = () => {
 
     return () => {
       unsubscribe();
-    }
+    };
   }, [id]);
 
   const handleSeeMoreClick = () => {
@@ -61,7 +66,9 @@ const Place = () => {
       if (id) {
         try {
           setIsLoading(true);
-          const {data}: {data?: {data: IPlace}} = await axios.get('https://getgooglemapsdetails-dp6fh5oj2q-uc.a.run.app/helloWorld?placeId=' + id);
+          const { data }: { data?: { data: IPlace } } = await axios.get(
+            'https://getgooglemapsdetails-dp6fh5oj2q-uc.a.run.app/helloWorld?placeId=' + id
+          );
           if (data?.data) {
             setPlaceData(data.data);
           }
@@ -76,51 +83,56 @@ const Place = () => {
 
   return (
     <div className={styles.mainContainer}>
-      <Header/>
+      <Header />
 
-        <div className={styles.main}>
-          <PageTitle title={'Place'} />
+      <div className={styles.main}>
+        <PageTitle title={'Place'} />
 
-          {!isLoading ? (
-            <>
-              {placeData?.imageUrl ? (
-                <div className={styles.post}>
-                  <div className={styles.container}>
-                    <div className={styles.postContainer}>
-                      <img src={placeData.imageUrl || ''} alt={'place image'} className={styles.postIMage}/>
+        <div className={styles.post}>
+          <div className={styles.container}>
+            <div className={styles.postContainer}>
+              {isLoading ? (
+                <Lottie animationData={AnimationData} />
+              ) : placeData?.imageUrl && placeData.articleText ? (
+                <>
+                  <img
+                    src={placeData.imageUrl || ''}
+                    alt={'place image'}
+                    className={styles.postIMage}
+                  />
 
-                      {placeData.articleText ? (
-                        <div className={styles.textContainer}>
-                          {isExpanded ? (
-                            <div>
-                              <div dangerouslySetInnerHTML={{__html: placeData.articleText}}/>
-                              <button onClick={handleSeeMoreClick}>See less</button>
-                            </div>
-                          ) : (
-                            <div>
-                              <div dangerouslySetInnerHTML={{__html: truncatedText.current}}/>
-                              {placeData.articleText.length > MAX_LENGTH && (
-                                <button onClick={handleSeeMoreClick}>See more</button>
-                              )}
-                            </div>
+                  {placeData.articleText ? (
+                    <div className={styles.textContainer}>
+                      {isExpanded ? (
+                        <div>
+                          <div dangerouslySetInnerHTML={{ __html: placeData.articleText }} />
+                          <button onClick={handleSeeMoreClick}>See less</button>
+                        </div>
+                      ) : (
+                        <div>
+                          <div dangerouslySetInnerHTML={{ __html: truncatedText.current }} />
+                          {placeData.articleText.length > MAX_LENGTH && (
+                            <button onClick={handleSeeMoreClick}>See more</button>
                           )}
                         </div>
-                      ) : null}
+                      )}
                     </div>
-                  </div>
-
-                  {id && (
-                    <PlaceCommentField placeId={id}/>
-                  )}
-
-                  {comments?.map(comment => <Comment key={comment.id} comment={comment} />)}
-                </div>
+                  ) : null}
+                </>
               ) : (
                 <h2 className={styles.empty}>There is no information about this place</h2>
               )}
+            </div>
+          </div>
+
+          {isLoading ? null : (
+            <>
+              {id && <PlaceCommentField placeId={id} />}
+              {comments?.map((comment) => <Comment key={comment.id} comment={comment} />)}
             </>
-          ) : <Skeleton className={styles.loader}/>}
+          )}
         </div>
+      </div>
     </div>
   );
 };
