9f747a04d53153a4984317c4f96d4cce65100699
vysochanskyi <visosensey@gmail.com>
2024-04-05 17:09:43 +0300

    fixed format date

--- src/utils/getDateToDisplay.ts
+++ src/utils/getDateToDisplay.ts
@@ -1,9 +1,9 @@
 export const getDateToDisplay = (date: string | number | Date) => {
   const newDate = new Date(date)
-    .toLocaleDateString(undefined, {
-      month: 'numeric',
+    .toLocaleDateString('en-US', {
+      month: '2-digit',
       year: 'numeric',
-      day: 'numeric',
+      day: '2-digit',
     })
     .split('/');
   return `${newDate[1]}/${newDate[0]}/${newDate[2]}`;
