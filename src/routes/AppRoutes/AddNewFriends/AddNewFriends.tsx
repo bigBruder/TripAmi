156b0b3a81fb291812f7375c21409cc4ea55fe12
vysochanskiy <visosensey@gmail.com>
2025-04-15 08:55:10 +0300

    test friends list

--- src/routes/AppRoutes/AddNewFriends/AddNewFriends.tsx
+++ src/routes/AppRoutes/AddNewFriends/AddNewFriends.tsx
@@ -219,6 +219,12 @@ const AddNewFriends: FC<AddNewFriendsProps> = ({ user, isFriend = false, isTabs 
     }
   }, [firestoreUser?.firebaseUid, facebookFriends, firestoreUser?.id, user?.id]);
 
+  console.log('facebook friends', facebookFriends.length);
+
+  console.log('firestore user', firestoreUser?.userFromFacebook);
+
+  console.log('close facebook', closeFacebook);
+
   if (user) {
     return users.length ? (
       <div className={styles.container}>
